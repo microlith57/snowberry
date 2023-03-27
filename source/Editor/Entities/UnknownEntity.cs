41172d44d3467d659b81f726560b8187a9a28df2
l-Luna <izzeldeen03@gmail.com>
2023-03-27 17:31:12 +0100

    unknown trigger rendering

--- source/Editor/Entities/UnknownEntity.cs
+++ source/Editor/Entities/UnknownEntity.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.Xna.Framework;
+using Monocle;
 
 namespace Snowberry.Editor.Entities;
 
 // Any entity that doesn't have its own plugin.
-public class UnknownEntity : Entity{
+public class UnknownEntity : Entity {
+    private static readonly Color TriggerColor = Calc.HexToColor("0c5f7a");
+
     public readonly Dictionary<string, object> Attrs = new();
     public bool LoadedFromTrigger = false;
+
+    private string triggerText = null;
 
     public override bool IsTrigger => LoadedFromTrigger;
 
@@ -15,5 +22,19 @@ public class UnknownEntity : Entity{
 
     public override object Get(string option) {
         return Attrs[option];
+    }
+
+    public override void Render() {
+        base.Render();
+
+        if (LoadedFromTrigger) {
+            Rectangle rect = new Rectangle((int)Position.X, (int)Position.Y, Width, Height);
+            Draw.Rect(rect, TriggerColor * 0.3f);
+            Draw.HollowRect(rect, TriggerColor);
+
+            triggerText ??= string.Join(" ", Regex.Split(char.ToUpper(Name[0]) + Name.Substring(1), @"(?=[A-Z])")).Trim();
+
+            Fonts.Pico8.Draw(triggerText, new Vector2(rect.X + rect.Width / 2f, rect.Y + rect.Height / 2f), Vector2.One, Vector2.One * 0.5f, Color.Black);
+        }
     }
 }
