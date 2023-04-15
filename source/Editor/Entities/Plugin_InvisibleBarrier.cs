c72c7737985f8a3ce2588e057b1ed6822e24cb7b
l-Luna <izzeldeen03@gmail.com>
2023-04-15 17:54:32 +0100

    invisible barrier plugin

---
+++ source/Editor/Entities/Plugin_InvisibleBarrier.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("invisibleBarrier")]
+public class Plugin_InvisibleBarrier : Entity {
+
+    public static readonly Color BarrierColour = Calc.HexToColor("666666");
+
+    public override int MinWidth => 8;
+    public override int MinHeight => 8;
+
+    public override void Render() {
+        base.Render();
+
+        Draw.Rect(Position, Width, Height, BarrierColour * 0.8f);
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("Invisible Barrier", "invisibleBarrier");
+    }
+}
