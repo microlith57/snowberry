02a2af4dfc8bab7a9e3324f140b1af6fe47f0a24
l-Luna <izzeldeen03@gmail.com>
2023-04-16 22:55:17 +0100

    moving platform // fix sinking platform texture field

---
+++ source/Editor/Entities/Plugin_MovingPlatform.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("movingPlatform")]
+public class Plugin_MovingPlatform : Entity {
+
+    // TODO: suggestions
+    [Option("texture")] public string Texture = "default";
+
+    public override int MinWidth => 16;
+    public override int MinNodes => 1;
+    public override int MaxNodes => 1;
+
+    public override void InitializeAfter() {
+        base.InitializeAfter();
+
+        Texture = Plugin_SinkingPlatform.GetVanillaLevelTexture() ?? Texture;
+    }
+
+    public override void Render() {
+        base.Render();
+
+        KeyValuePair<Color, Color> colours = GetLineColours();
+        Color lineEdgeColor = colours.Key, lineInnerColor = colours.Value;
+
+        var end = Nodes[0] + new Vector2(Width / 2f, 4);
+        var linePos = Position + new Vector2(Width / 2f, 4);
+        Vector2 diff = (end - linePos).SafeNormalize();
+        Vector2 diffP = new Vector2(-diff.Y, diff.X);
+        Draw.Line(linePos - diff - diffP, end + diff - diffP, lineEdgeColor);
+        Draw.Line(linePos - diff, end + diff, lineEdgeColor);
+        Draw.Line(linePos - diff + diffP, end + diff + diffP, lineEdgeColor);
+        Draw.Line(linePos, end, lineInnerColor);
+
+        Plugin_SinkingPlatform.DrawPlatform(Nodes[0], Width, Texture, Color.White * 0.4f);
+        Plugin_SinkingPlatform.DrawPlatform(Position, Width, Texture);
+    }
+
+    public static KeyValuePair<Color, Color> GetLineColours() {
+        return Editor.VanillaLevelID switch {
+            4 => new(Calc.HexToColor("a4464a"), Calc.HexToColor("86354e")),
+            _ => new(Calc.HexToColor("2a1923"), Calc.HexToColor("160b12"))
+        };
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        yield return RectOnRelative(new(Width, 8));
+        yield return RectOnAbsolute(new(Width, 8), Nodes[0]);
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("Moving Platform", "movingPlatform");
+    }
+}
--- source/Editor/Entities/Plugin_SinkingPlatform.cs
+++ source/Editor/Entities/Plugin_SinkingPlatform.cs
@@ -12,7 +12,7 @@ public class Plugin_SinkingPlatform : Entity {
     public static readonly Color LineInnerColour = Calc.HexToColor("160b12");
 
     // TODO: suggestions
-    [Option("texture")] private string Texture = "default";
+    [Option("texture")] public string Texture = "default";
 
     public override int MinWidth => 16;
 
@@ -28,21 +28,26 @@ public class Plugin_SinkingPlatform : Entity {
         Draw.Rect(X - 1 + Width / 2f, Y + 4, 3, (Room?.Height ?? 15) * 8, LineEdgeColour);
         Draw.Rect(X + Width / 2f, Y + 4, 1, (Room?.Height ?? 15) * 8, LineInnerColour);
 
-        MTexture tex = GFX.Game["objects/woodPlatform/" + Texture];
+        DrawPlatform(Position, Width, Texture);
+    }
+
+    public static void DrawPlatform(Vector2 position, int width, string texture, Color? c = default) {
+        Color color = c ?? Color.White;
+        MTexture tex = GFX.Game["objects/woodPlatform/" + texture];
         MTexture
             start = tex.GetSubtexture(0, 0, 8, 8),
             mid = tex.GetSubtexture(8, 0, 8, 8),
             centre = tex.GetSubtexture(16, 0, 8, 8),
             end = tex.GetSubtexture(24, 0, 8, 8);
 
-        start.Draw(Position);
-        for (int i = 8; i < Width - 8; i += 8)
-            mid.Draw(Position + new Vector2(i, 0));
-        end.Draw(Position + new Vector2(Width - 8, 0));
-        centre.Draw(Position + new Vector2(Width / 2f - 4, 0));
+        start.Draw(position, Vector2.Zero, color);
+        for(int i = 8; i < width - 8; i += 8)
+            mid.Draw(position + new Vector2(i, 0), Vector2.Zero, color);
+        end.Draw(position + new Vector2(width - 8, 0), Vector2.Zero, color);
+        centre.Draw(position + new Vector2(width / 2f - 4, 0), Vector2.Zero, color);
     }
 
-    private static string GetVanillaLevelTexture() {
+    public static string GetVanillaLevelTexture() {
         return Editor.VanillaLevelID switch {
             4 => "cliffside",
             _ => null
