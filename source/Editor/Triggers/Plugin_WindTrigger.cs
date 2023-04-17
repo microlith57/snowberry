5e615c04914fbbb055bf36e5e74b6918b65ce5cb
l-Luna <izzeldeen03@gmail.com>
2023-04-17 17:09:59 +0100

    wind trigger plugin

---
+++ source/Editor/Triggers/Plugin_WindTrigger.cs
@@ -0,0 +1,19 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("windTrigger")]
+public class Plugin_WindTrigger : Trigger {
+
+    [Option("pattern")] public WindController.Patterns Pattern = WindController.Patterns.None;
+
+    public override void Render() {
+        base.Render();
+        Fonts.Pico8.Draw($"({Pattern})", Center + Vector2.UnitY * 6, Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Wind Trigger", "windTrigger");
+    }
+}
