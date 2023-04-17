590d3ec56ccf93a31eca59bc4d578d2668a264e8
l-Luna <izzeldeen03@gmail.com>
2023-04-17 17:27:23 +0100

    cliffside flag plugin

---
+++ source/Editor/Entities/Plugin_CliffsideFlag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("cliffside_flag")]
+public class Plugin_CliffsideFlag : Entity {
+
+    // TODO: bound in 0-10
+    [Option("index")] public int Index = 0;
+
+    public override void Render() {
+        base.Render();
+
+        GetTexture().Draw(Position);
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        var tex = GetTexture();
+        yield return RectOnRelative(new(tex.Width, tex.Height));
+    }
+
+    public MTexture GetTexture() => GFX.Game.GetAtlasSubtexturesAt("scenery/cliffside/flag", Index);
+
+    public static void AddPlacements() {
+        Placements.Create("Cliffside Flag", "cliffside_flag");
+    }
+}
