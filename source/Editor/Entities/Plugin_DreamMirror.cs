929400322a7abf80825a520ea65f8e4d9c806184
l-Luna <izzeldeen03@gmail.com>
2023-08-16 01:46:53 +0300

    white block plugin // fix dream mirror selection

--- source/Editor/Entities/Plugin_DreamMirror.cs
+++ source/Editor/Entities/Plugin_DreamMirror.cs
@@ -1,8 +1,9 @@
-﻿using Celeste;
+﻿using System.Collections.Generic;
+using Celeste;
 using Microsoft.Xna.Framework;
 using Monocle;
 
-namespace Snowberry.Editor.Entities; 
+namespace Snowberry.Editor.Entities;
 
 [Plugin("dreammirror")]
 public class Plugin_DreamMirror : Entity {
@@ -18,6 +19,10 @@ public class Plugin_DreamMirror : Entity {
         base.Render();
         mirror.DrawJustified(Position, new Vector2(0.5f, 1.0f));
         frame.DrawJustified(Position, new Vector2(0.5f, 1.0f));
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        yield return RectOnRelative(new(62, 37), justify: new(0.5f, 1));
     }
 
     public static void AddPlacements() {
---
+++ source/Editor/Entities/Plugin_WhiteBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("whiteblock")]
+public class Plugin_WhiteBlock : Entity {
+    private MTexture sprite;
+
+    public override void Initialize() {
+        base.Initialize();
+        sprite = GFX.Game["objects/whiteblock"];
+    }
+
+    public override void Render() {
+        base.Render();
+        sprite.Draw(Position);
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        yield return RectOnRelative(new(48, 24));
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("White Block", "whiteblock");
+    }
+}
