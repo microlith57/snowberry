3249a6b80c9fc00feb7b143df8a20e0a3d4a5e30
l-Luna <izzeldeen03@gmail.com>
2023-08-16 02:02:34 +0300

    temple eye plugin

---
+++ source/Editor/Entities/Plugin_TempleEye.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Celeste;
+using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("templeEye")]
+public class Plugin_TempleEye : Entity {
+
+    // TODO: change depth (unimplemented) to render on top of FG tiles when present
+
+    public override void Render() {
+        base.Render();
+
+        string pfix = (Room == null || Room.GetFgTile(Position) != '0') ? "fg" : "bg";
+        GFX.Game[$"scenery/temple/eye/{pfix}_eye"].DrawCentered(Position);
+        GFX.Game[$"scenery/temple/eye/{pfix}_pupil"].DrawCentered(Position);
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        yield return RectOnRelative(new(12, 13), justify: new(0.5f));
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("Temple Eye", "templeEye");
+    }
+}
