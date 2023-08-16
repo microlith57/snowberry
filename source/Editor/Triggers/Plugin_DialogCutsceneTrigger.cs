47a47079b8692174063655359f6bcb2bd1d44cdc
l-Luna <izzeldeen03@gmail.com>
2023-08-16 14:27:49 +0300

    textbox & minitextbox trigger plugins

---
+++ source/Editor/Triggers/Plugin_DialogCutsceneTrigger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("everest/dialogTrigger")]
+public class Plugin_DialogCutsceneTrigger : Trigger{
+
+    [Option("dialogId")] public string DialogId = "";
+    [Option("onlyOnce")] public bool OnlyOnce = true;
+    [Option("endLevel")] public bool EndLevel = false;
+    [Option("deathCount")] public int DeathCount = -1;
+
+    public override void Render() {
+        base.Render();
+        var str = $"\"{DialogId}\"";
+        Fonts.Pico8.Draw(str, Center + new Vector2(0, 6), Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Dialog Cutscene Trigger (Everest)", "everest/dialogTrigger");
+    }
+}
---
+++ source/Editor/Triggers/Plugin_MiniTextboxTrigger.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("minitextboxTrigger")]
+public class Plugin_MiniTextboxTrigger : Trigger {
+
+    [Option("mode")] public Modes Mode = Modes.OnPlayerEnter;
+    [Option("dialog_id")] public string DialogId = "";
+    [Option("only_once")] public bool OnlyOnce = false;
+    [Option("death_count")] public int DeathCount = -1;
+
+    public override void Render() {
+        base.Render();
+        var str = $"\"{DialogId}\"";
+        Fonts.Pico8.Draw(str, Center + new Vector2(0, 6), Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Mini Textbox Trigger", "minitextboxTrigger");
+    }
+
+    public enum Modes{
+        OnPlayerEnter,
+        OnLevelStart,
+        OnTheoEnter
+    }
+}
