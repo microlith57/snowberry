fa9789b5456e0aa1fc64633315d61a290938da48
l-Luna <izzeldeen03@gmail.com>
2023-08-16 14:44:16 +0300

    spawn facing trigger

--- source/Editor/Entities/Plugin_Player.cs
+++ source/Editor/Entities/Plugin_Player.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Celeste;
 using Microsoft.Xna.Framework;
+using Snowberry.Editor.Triggers;
 
 namespace Snowberry.Editor.Entities;
 
 [Plugin("player")]
-public class Plugin_Player : Entity {
-    public override void Render() {
-        base.Render();
+public class Plugin_Player : Entity{
 
-        FromSprite("player", "sitDown")?.DrawCentered(Position - Vector2.UnitY * 16);
+    public override void Render(){
+        base.Render();
+        int xScale = GetSpawnFacing() == Facings.Left ? -1 : 1;
+        FromSprite("player", "sitDown")?.DrawCentered(Position - Vector2.UnitY * 16, Color.White, new Vector2(xScale, 1));
     }
 
-    protected override IEnumerable<Rectangle> Select() {
+    protected override IEnumerable<Rectangle> Select(){
         yield return RectOnRelative(new(13, 17), position: new(-1, 0), justify: new(0.5f, 1));
     }
 
-    public static void AddPlacements() {
+    public static void AddPlacements(){
         Placements.Create("Spawn Point", "player");
+    }
+
+    protected Facings? GetSpawnFacing(){
+        return Room != null && Room.TrackedEntities.ContainsKey(typeof(Plugin_SpawnFacingTrigger))
+                ? Room.TrackedEntities[typeof(Plugin_SpawnFacingTrigger)]
+                    .OfType<Plugin_SpawnFacingTrigger>()
+                    .FirstOrDefault(e => e.Bounds.Intersects(Select().First()))?.Facing
+                : null;
     }
 }
---
+++ source/Editor/Triggers/Plugin_SpawnFacingTrigger.cs
@@ -0,0 +1,23 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("spawnFacingTrigger")]
+public class Plugin_SpawnFacingTrigger : Trigger{
+
+    [Option("facing")] public Facings Facing = Facings.Left;
+
+    public Plugin_SpawnFacingTrigger(){
+        Tracked = true;
+    }
+
+    public override void Render(){
+        base.Render();
+        Fonts.Pico8.Draw(Facing.ToString().ToLower(), Center + new Vector2(0, 6), Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements(){
+        Placements.Create("Spawn Facing Trigger", "spawnFacingTrigger");
+    }
+}
