1f5462914c5b8a6ae659c262c9536724f06ead54
l-Luna <izzeldeen03@gmail.com>
2023-08-17 13:29:32 +0300

    proper feather selection

--- source/Editor/Entities/Plugin_Feather.cs
+++ source/Editor/Entities/Plugin_Feather.cs
@@ -1,23 +1,26 @@
-﻿using Celeste;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Monocle;
 using System.Collections.Generic;
 
-namespace Snowberry.Editor.Entities; 
+namespace Snowberry.Editor.Entities;
 
 [Plugin("infiniteStar")]
-public class Plugin_Feather : Entity {
+public class Plugin_Feather : Entity{
     [Option("shielded")] public bool Shielded;
     [Option("singleUse")] public bool OneUse;
 
-    public override void Render() {
+    public override void Render(){
         base.Render();
         FromSprite("flyFeather", "loop")?.DrawCentered(Position);
-        if (Shielded)
+        if(Shielded)
             Draw.Circle(Position, 12f, Color.White, 5);
     }
 
-    public static void AddPlacements() {
+    protected override IEnumerable<Rectangle> Select(){
+        yield return RectOnRelative(Shielded ? new(22) : new(18), justify: new(0.5f));
+    }
+
+    public static void AddPlacements(){
         Placements.Create("Feather", "infiniteStar");
         Placements.Create("Feather (Shielded)", "infiniteStar", new Dictionary<string, object>() { { "shielded", true } });
     }
