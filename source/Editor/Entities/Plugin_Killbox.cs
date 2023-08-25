c891f9e004bcf5a486d6bd3e15240c418b1d931b
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 09:15:49 -0700

    Add killbox plugin
    not tested ingame due to assembly load issues but like i'd be very surprised if this didn't work

---
+++ source/Editor/Entities/Plugin_Killbox.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("killbox")]
+public class Plugin_Killbox : Entity {
+
+    public static readonly Color killboxColour = Calc.HexToColor("CC6666");
+
+    public override int MinWidth => 8;
+    public override int MinHeight => 32;
+
+    public override void Render() {
+        base.Render();
+
+        Draw.Rect(Position, Width, Height, killboxColour * 0.8f);
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("Killbox", "killbox");
+    }
+}
