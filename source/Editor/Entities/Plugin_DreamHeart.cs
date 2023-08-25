80ada8bdba76277793935603cd75a02edc471451
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 12:01:07 -0700

    Add dream heart plugin
    shoutout to the crystal heart plugin not being named after either the common name or internal name

---
+++ source/Editor/Entities/Plugin_DreamHeart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("dreamHeartGem")]
+public class Plugin_DreamHeart : Entity {
+
+    public override void Render() {
+        base.Render();
+
+        FromSprite("heartgem0", "idle")?.DrawCentered(Position, Color.White, new Vector2(1, 1));
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        yield return RectOnRelative(new(16, 16), justify: new(0.5f, 0.5f));
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("Crystal Heart (Dream)", "dreamHeartGem");
+    }
+}
