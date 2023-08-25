104c74f204e07750120c30fc46c56e192625f98a
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 10:33:36 -0700

    Add basic bumper plugin
    For the future: make sprite better, update sprite to reflect core mode

---
+++ source/Editor/Entities/Plugin_Bumper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("bigSpinner")]
+public class Plugin_Bumper : Entity {
+    
+    public override void Render() {
+        base.Render();
+
+        FromSprite("bumper", "idle")?.DrawCentered(Position, Color.White, new Vector2(1, 1));
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        yield return RectOnRelative(new(22, 22), justify: new(0.5f, 0.5f));
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("Bumper", "bigSpinner");
+    }
+}
