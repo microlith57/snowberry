adb3a29a4e1949780f2fec9e990e94716593214d
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 16:45:09 -0700

    Add Crystal Shatter Trigger
    another thing i'm very surprised is vanilla (or everest but whatever)

---
+++ source/Editor/Triggers/Plugin_CrystalShatterTrigger.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using static Celeste.Mod.Entities.CrystalShatterTrigger;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("everest/crystalShatterTrigger")]
+public class Plugin_CrystalShatterTrigger : Trigger {
+    [Option("mode")] public Modes Mode = Modes.All;
+
+    public override void Render() {
+        base.Render();
+        var str = $"({Mode})";
+        Fonts.Pico8.Draw(str, Center + Vector2.UnitY * 6, Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Crystal Shatter Trigger (Everest)", "everest/crystalShatterTrigger");
+    }
+}
