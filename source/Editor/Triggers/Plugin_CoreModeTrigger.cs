71c41f2a9d9d25343c779f12e8257ea293384c5d
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 16:44:50 -0700

    Add Core Mode Trigger

---
+++ source/Editor/Triggers/Plugin_CoreModeTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using static Celeste.Trigger;
+using static Celeste.Session;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("everest/coreModeTrigger")]
+public class Plugin_CoreModeTrigger : Trigger {
+    [Option("mode")] public Celeste.Session.CoreModes CoreMode = Celeste.Session.CoreModes.None;
+    [Option("playEffects")] public bool PlayEffects = true;
+
+    public override void Render() {
+        base.Render();
+        var str = $"({CoreMode})";
+        Fonts.Pico8.Draw(str, Center + Vector2.UnitY * 6, Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Core Mode Trigger (Everest)", "everest/coreModeTrigger");
+    }
+}
