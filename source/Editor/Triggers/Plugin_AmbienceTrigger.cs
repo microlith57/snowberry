5392e91cac2a1c32a205b714005a952cf9000f29
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 15:09:26 -0700

    Add various music triggers
    Includes Alt Music Trigger, Ambience Trigger, and Ambience Volume Trigger. all work in-editor

---
+++ source/Editor/Triggers/Plugin_AmbienceTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("everest/ambienceTrigger")]
+public class Plugin_AmbienceTrigger : Trigger {
+
+    [Option("track")] public string Track = "";
+    [Option("resetOnLeave")] public bool ResetOnLeave = true;
+
+    public override void Render() {
+        base.Render();
+
+        var str = (Track == "") ? "" : $"({Track})";
+        Fonts.Pico8.Draw(str, Center + Vector2.UnitY * 6, Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Ambience Trigger (Everest)", "everest/ambienceTrigger");
+    }
+}
---
+++ source/Editor/Triggers/Plugin_AmbienceVolumeTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("everest/AmbienceVolumeTrigger")]
+public class Plugin_AmbienceVolumeTrigger : Trigger {
+    [Option("direction")] public Celeste.Trigger.PositionModes PositionMode = Celeste.Trigger.PositionModes.NoEffect;
+    [Option("from")] public float From = 0;
+    [Option("to")] public float To = 0;
+
+    public override void Render() {
+        base.Render();
+
+        var str = From == To ? $"({To})" : $"({From} -> {To})";
+        Fonts.Pico8.Draw(str, Center + Vector2.UnitY * 6, Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Ambience Volume Trigger (Everest)", "everest/AmbienceVolumeTrigger");
+    }
+}
