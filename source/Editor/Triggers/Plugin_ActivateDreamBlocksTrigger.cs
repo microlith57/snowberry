6a7c4b921fe611708e23247e75e7e44a8ef85de9
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 15:09:40 -0700

    Add Activate Dream Blocks trigger

---
+++ source/Editor/Triggers/Plugin_ActivateDreamBlocksTrigger.cs
@@ -0,0 +1,13 @@
+﻿namespace Snowberry.Editor.Triggers;
+
+[Plugin("everest/activateDreamBlocksTrigger")]
+public class Plugin_ActivateDreamBlocksTrigger : Trigger {
+
+    [Option("fullRoutine")] public bool FullRoutine = false;
+    [Option("activate")] public bool Activate = true;
+    [Option("fastAnimation")] public bool FastAnimation = false;
+
+    public new static void AddPlacements() {
+        Placements.Create("Activate Dream Blocks Trigger (Everest)", "everest/activateDreamBlocksTrigger");
+    }
+}
