e1d24db6c0578072c20974984bee7e4480000039
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 14:10:09 -0700

    Add Detach Followers Trigger
    now with node!

---
+++ source/Editor/Triggers/Plugin_DetachFollowersTrigger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("detachFollowersTrigger")]
+public class Plugin_DetachFollowersTrigger : Trigger {
+
+    [Option("global")] public bool Global = true;
+
+    public override int MinNodes => 1;
+    public override int MaxNodes => 1;
+
+    public override void Render() {
+        base.Render();
+
+        DrawUtil.DottedLine(Center, Nodes[0], Color.White * 0.5f, 8, 4);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Detach Followers Trigger", "detachFollowersTrigger");
+    }
+}
