7e5883ea46510d07353a2b780654eb925ba3b112
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-25 18:08:07 -0700

    Add Music Layer Trigger

---
+++ source/Editor/Triggers/Plugin_MusicLayerTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Triggers;
+
+[Plugin("everest/musicLayerTrigger")]
+public class Plugin_MusicLayerTrigger : Trigger {
+
+    [Option("track")] public string Track = "";
+    [Option("enable")] public bool Enable = false;
+
+    public override void Render() {
+        base.Render();
+
+        var str = (Track == "") ? "" : $"(\"{Track}\", {Enable})";
+        Fonts.Pico8.Draw(str, Center + Vector2.UnitY * 6, Vector2.One, new(0.5f), Color.Black);
+    }
+
+    public new static void AddPlacements() {
+        Placements.Create("Music Layer (Everest)", "everest/musicLayerTrigger");
+    }
+}
