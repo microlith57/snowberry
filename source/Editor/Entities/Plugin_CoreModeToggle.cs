f9453320b5601a4fd4145508bfa42fed9aba42af
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-27 16:56:09 -0700

    Add Core Mode Toggle and Fling Bird
    Lots of potential for improvement and fancy stuff in Fling Bird, check that out

---
+++ source/Editor/Entities/Plugin_CoreModeToggle.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Celeste;
+using Microsoft.Xna.Framework;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("coreModeToggle")]
+public class Plugin_CoreModeToggle : Entity {
+
+    [Option("mode")] public CoreToggleMode Mode = CoreToggleMode.Both;
+    [Option("persistent")] public bool Persistent = false;
+
+    public override void Render() {
+        base.Render();
+
+        string postfix;
+        switch (Mode) {
+            case CoreToggleMode.Both: {
+                postfix = "01";
+                break;
+            }
+            case CoreToggleMode.OnlyFire: {
+                postfix = "15";
+                break;
+            }
+            case CoreToggleMode.OnlyIce: {
+                postfix = "13";
+                break;
+            }
+            default:
+                postfix = "01";
+                break;
+        }
+
+        GFX.Game[$"objects/coreFlipSwitch/switch{postfix}"].DrawCentered(Position);
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+
+        float iceModSize = Mode == CoreToggleMode.OnlyIce ? 3 : 0;
+        float iceModPos = Mode == CoreToggleMode.OnlyIce ? -8.5f : 0;
+        yield return RectOnRelative(new(16, 20 + iceModSize), position: new(0, 5 + iceModPos), justify: new(0.5f, 0.5f));
+    }
+
+    public override void SaveAttrs(BinaryPacker.Element e) {
+        if (Mode == CoreToggleMode.OnlyFire)
+            e.Attributes["onlyFire"] = true;
+        if (Mode == CoreToggleMode.OnlyIce)
+            e.Attributes["onlyIce"] = true;
+    }
+    public static void AddPlacements() {
+        Placements.Create("Core Mode Toggle", "coreModeToggle");
+    }
+
+    public enum CoreToggleMode {
+        Both,
+        OnlyFire,
+        OnlyIce
+    }
+}
