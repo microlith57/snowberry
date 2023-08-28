4c8b2b4ef20e7724b20944ed81c3ca2d9c2b9238
l-Luna <izzeldeen03@gmail.com>
2023-08-28 12:40:51 +0300

    fix fling bird next bird highlighting

--- source/Editor/Entities/Plugin_FlingBird.cs
+++ source/Editor/Entities/Plugin_FlingBird.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Celeste;
 using System.Collections.Generic;
+using Monocle;
 
-namespace Snowberry.Editor.Entities; 
+namespace Snowberry.Editor.Entities;
 
 // So I am the greatest coder alive, which is why my enemies (the compiler) thwarted me when I tried to make birds showing what bird is next in the sequence with a dotted line work.
 // Relevant code is commented out below. If you feel like taking a look, please do i will love you forever
@@ -19,25 +20,30 @@ public class Plugin_FlingBird : Entity {
     public override void Render() {
         base.Render();
 
-        GFX.Game["characters/bird/hover04"].DrawCentered(Position);
+        MTexture sprite = GFX.Game["characters/bird/hover04"];
+        sprite.DrawCentered(Position);
+
+        foreach (var node in Nodes)
+            sprite.DrawCentered(node, Color.White * 0.5f);
     }
 
     public override void HQRender() {
         base.HQRender();
 
         Vector2 prev = Position;
-        if (Nodes.Count != 0) { 
+        if (Nodes.Count != 0) {
             foreach (Vector2 node in Nodes) {
                 DrawUtil.DottedLine(prev, node, Color.White * 0.5f, 8, 4);
                 prev = node;
             }
         }
-        /**
-        // Draw a line from either the bird's position or the last node to the next bird in the sequence.
-        if (Room.TrackedEntities[typeof(Plugin_FlingBird)].Count > 1) {
-            Vector2 startPos = (Nodes.Count == 0) ? Position : Nodes[Nodes.Count - 1];
-            DrawUtil.DottedLine(startPos, NextBirdPos(), Color.White, 8, 4);
-        } **/
+
+        if (Room.TrackedEntities[typeof(Plugin_FlingBird)] is { Count: > 1 }) {
+            Vector2 startPos = Nodes.Count == 0 ? Position : Nodes[Nodes.Count - 1];
+            Vector2? next = NextBirdPos();
+            if(next != null)
+                DrawUtil.DottedLine(startPos, next.Value, Color.Blue, 8, 4);
+        }
     }
 
     protected override IEnumerable<Rectangle> Select() {
@@ -50,22 +56,22 @@ public class Plugin_FlingBird : Entity {
         Placements.Create("Fling Bird", "flingBird");
     }
 
-    /**
     // Calculate the position of the bird closest to the right of the current bird.
     protected Vector2? NextBirdPos() {
-        float? mindx = null;
-        Vector2 closestPos = new Vector2(-1, -1);
+        float? minDx = null;
+        Vector2? closestPos = null;
 
-        for (int i = 0; i < Room.TrackedEntities[typeof(Plugin_FlingBird)].Count; i++) {
-            float dx = Room.TrackedEntities[typeof(Plugin_FlingBird)][i].Position.X - this.Position.X;
-            if (mindx == null || (dx < mindx && dx > 0)) {
-                closestPos = Room.TrackedEntities[typeof(Plugin_FlingBird)][i].Position;
-                mindx = dx;
+        foreach(var entity in Room.TrackedEntities[typeof(Plugin_FlingBird)]){
+            if(entity == this)
+                continue;
+
+            float dx = entity.Position.X - Position.X;
+            if (dx > 0 && (minDx == null || dx < minDx)) {
+                closestPos = entity.Position;
+                minDx = dx;
             }
         }
 
-        if (mindx > 0) {
-            return closestPos;
-        } else return Position;
-    } **/
+        return closestPos;
+    }
 }
