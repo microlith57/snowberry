52071904f2f9d1bcf1487c46313555acf53e78f8
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2023-08-28 11:44:56 -0700

    Add Golden Block
    mildly buggy oops

---
+++ source/Editor/Entities/Plugin_GoldenBlock.cs
@@ -0,0 +1,28 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
+using Snowberry.Editor.Entities.Util;
+
+namespace Snowberry.Editor.Entities;
+
+[Plugin("goldenBlock")]
+public class Plugin_GoldBlock : Entity {
+
+    public static readonly EditorNinePatch blockTex = new(GFX.Game["objects/goldblock"]);
+
+    public override int MinWidth => 16;
+    public override int MinHeight => 16;
+
+    public override void Render() {
+        base.Render();
+
+        blockTex.Draw(Position, Width, Height, Color.White);
+        MTexture centerTex = GFX.Game["collectables/goldberry/idle00"];
+        centerTex.DrawCentered(new(Position.X + (Width / 2), Position.Y + (Height / 2)));
+
+    }
+
+    public static void AddPlacements() {
+        Placements.Create("Golden Block", "goldenBlock");
+    }
+}
