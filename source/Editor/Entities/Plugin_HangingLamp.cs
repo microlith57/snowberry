a7cf7f5e8a3bf6543a58ea1f097388698156e1d1
l-Luna <izzeldeen03@gmail.com>
2023-09-02 12:56:21 +0300

    x2 combob

--- source/Editor/Entities/Plugin_HangingLamp.cs
+++ source/Editor/Entities/Plugin_HangingLamp.cs
@@ -31,6 +31,6 @@ public class Plugin_HangingLamp : Entity {
     }
 
     public static void AddPlacements() {
-        Placements.Create("Hanging Lamp", "hangingLamp");
+        Placements.Create("Hanging Lamp", "hanginglamp");
     }
 }
