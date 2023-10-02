021d5a24eee55ebb5f3b3bab64ab298baf68e20e
l-Luna <izzeldeen03@gmail.com>
2023-10-02 15:46:51 +0100

    fix `drawableRectangle.fromRectangle` with table args

--- LoennHelpers/structs/drawable_rectangle.lua
+++ LoennHelpers/structs/drawable_rectangle.lua
@@ -6,6 +6,7 @@ end
 -- end snowberry header
 
 local drawableSprite = require("structs.drawable_sprite")
+local utils = require("utils")
 
 local texName = "snowberry/1x1"
 
@@ -15,6 +16,16 @@ drawableRectangle.tintingPixelTexture = texName
 
 -- for now we just Don't
 function drawableRectangle.fromRectangle(mode, x, y, width, height, color, secondaryColor)
+    if type(x) == "table" then -- thanks loenn
+        color = utils.getColor(y)
+        secondaryColor = utils.getColor(width)
+
+        height = x.height or x[4]
+        width = x.width or x[3]
+        y = x.y or x[2]
+        x = x.x or x[1]
+    end
+    
     local sprite = drawableSprite.fromTexture(texName, { x = x, y = y, color = color })
     sprite:setScale(width, height)
     sprite:setJustification(0, 0)
