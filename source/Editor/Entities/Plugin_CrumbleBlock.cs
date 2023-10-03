4b9e3cd814da3cb1d08e9290dbb95ac1fdd7a545
l-Luna <izzeldeen03@gmail.com>
2023-10-03 14:43:44 +0100

    better crumble block selection box

--- source/Editor/Entities/Plugin_CrumbleBlock.cs
+++ source/Editor/Entities/Plugin_CrumbleBlock.cs
@@ -2,11 +2,12 @@
 using Microsoft.Xna.Framework;
 using Monocle;
 using System;
+using System.Collections.Generic;
 
 namespace Snowberry.Editor.Entities;
 
 [Plugin("crumbleBlock")]
-public class Plugin_CrumblePlatform : Entity {
+public class Plugin_CrumbleBlock : Entity {
     [Option("texture")] public string Texture = null;
 
     public override int MinWidth => 8;
@@ -21,6 +22,10 @@ public class Plugin_CrumblePlatform : Entity {
             int num2 = (int)((Math.Abs(X) + (float)j) / 8f) % 4;
             mTexture2.GetSubtexture(num2 * 8, 0, 8, 8).DrawCentered(new Vector2(4 + j + X, 4f + Y));
         }
+    }
+
+    protected override IEnumerable<Rectangle> Select() {
+        yield return RectOnRelative(new(Width, 8));
     }
 
     public static void AddPlacements() {
