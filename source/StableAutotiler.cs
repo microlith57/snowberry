adf038fb21564a2e60a48b592865226eca687de1
l-Luna <izzeldeen03@gmail.com>
2023-10-03 14:57:10 +0100

    improve stable tile rng

--- source/StableAutotiler.cs
+++ source/StableAutotiler.cs
@@ -74,5 +74,31 @@ public static class StableAutotiler {
         };
     }
 
-    private static MTexture Pick(List<MTexture> choices, int x2, int y2) => choices[(x2 * 84 ^ y2) % choices.Count];
+    private static ulong mod(ulong x, ulong m) => (x%m + m)%m;
+    private static ulong pack(int x, int y) => (((ulong)x) << 32) + (ulong)y;
+    private static MTexture Pick(List<MTexture> choices, int x2, int y2) => choices[(int)mod(splitmix64(pack(x2, y2)), (ulong)choices.Count)];
+
+    #region Splitmix64
+    /*  Written in 2015 by Sebastiano Vigna (vigna@acm.org)
+
+    To the extent possible under law, the author has dedicated all copyright
+    and related and neighboring rights to this software to the public domain
+    worldwide. This software is distributed without any warranty.
+
+    See <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+    This is a fixed-increment version of Java 8's SplittableRandom generator
+    See http://dx.doi.org/10.1145/2714064.2660195 and
+    http://docs.oracle.com/javase/8/docs/api/java/util/SplittableRandom.html
+
+    It is a very fast generator passing BigCrush, and it can be useful if
+    for some reason you absolutely want 64 bits of state.
+    */
+    private static ulong splitmix64(ulong seed) {
+        ulong z = seed + 0x9e3779b97f4a7c15;
+        z = (z ^ z >> 30) * 0xbf58476d1ce4e5b9;
+        z = (z ^ z >> 27) * 0x94d049bb133111eb;
+        return z ^ z >> 31;
+    }
+    #endregion
 }
