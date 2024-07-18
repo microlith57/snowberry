b915a9ee67d30d7cd467358cff9d6084a9c85816
microlith57 <microlith57@gmail.com>
2024-07-18 14:18:06 +1200

    trim celeste path from surgery ui, so it takes up less space

--- source/Surgery/Surgery.cs
+++ source/Surgery/Surgery.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using Celeste;
+using Celeste.Mod;
 using Microsoft.Xna.Framework;
 using Monocle;
 using Snowberry.UI;
@@ -24,6 +25,9 @@ public class Surgery(string path, Element elem) : UIScene {
     }
 
     internal void SurgeryUi() {
+        if (path.StartsWith(Everest.PathGame))
+            path = path[Everest.PathGame.Length..].TrimStart('/');
+
         UIElement topBar = new() {
             Background = Color.DarkRed,
             Width = UI.Width,
